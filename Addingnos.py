5b28c85342f0f65314efa50027c1b5f323f6ba35
Banu2013 <banumathi@baavatech.com>
2026-02-27 16:43:50 +0530

    Add files via upload

---
+++ Addingnos.py
@@ -0,0 +1,38 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 2+2
+4
+>>> #Addingtwo numbers
+>>> a=10
+>>> b=20
+>>> c=a+b
+>>> print(c)
+30
+>>> print(a)
+10
+>>> 
+>>> print(b)
+20
+>>> b=30
+>>> print(b)
+30
+>>> print(c)
+30
+>>> c=a+b
+>>> print(c)
+40
+>>> print(c)
+40
+>>> a=40
+>>> print(c)
+40
+>>> c=a+b
+>>> print(c)
+70
+>>> #adding numbers by user input
+>>> a=int(input("Enter first number:"))
+... b=int(input("enter second number:"))
+SyntaxError: multiple statements found while compiling a single statement
+>>> c=a+b
+>>> print("Answer is:",c)
+Answer is: 70
